aed77e916a057195cb1e88cbc03d0148d33a86a7
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-11-14 10:41:29 -0600

    added field names

--- root/app/models.py
+++ root/app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+
 class User(models.Model):
-class Set(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=64)
+
+
+class CardSet(models.Model):
+    name = models.CharField(max_length=150)
+    authorId = models.BigIntegerField
+
+
 class Flashcard(models.Model):
+    cardSetId = models.BigIntegerField
+    front = models.CharField(max_length=255)
+    back = models.TextField(max_length=1024)
+
+
 class Favorite(models.Model):
+    userId = models.BigIntegerField
+    cardSetId = models.BigIntegerField
 
