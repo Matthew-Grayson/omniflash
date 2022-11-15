e81642a0c144697d88eebf22b0c5577fdb9fc1cc
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-11-15 16:23:52 -0600

    added category models

--- root/app/models.py
+++ root/app/models.py
@@ -5,20 +5,26 @@ class User(models.Model):
     username = models.CharField(max_length=150)
     password = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'omniflash_users'
 
 
 class CardSet(models.Model):
     name = models.CharField(max_length=150)
-    authorId = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'omniflash_card_sets'
 
 
 class Flashcard(models.Model):
-    cardSetId = models.ForeignKey(CardSet, on_delete=models.CASCADE)
+    card_set = models.ForeignKey(CardSet, on_delete=models.CASCADE)
     front = models.CharField(max_length=255)
     back = models.TextField(max_length=1024)
 
@@ -27,8 +33,24 @@ class Flashcard(models.Model):
 
 
 class Favorite(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    cardSetId = models.ForeignKey(CardSet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_set = models.ForeignKey(CardSet, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'omniflash_favorites'
+
+
+class Category(models.Model):
+    name = models.enums
+
+    class Meta:
+        db_table = 'omniflash_categories'
+
+
+class SetCategory(models.Model):
+    card_set = models.ForeignKey(CardSet, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'omniflash_set_category'
+
